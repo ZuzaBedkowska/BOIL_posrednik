41edbbfd56e7e1afe2d0eab2a88acfa595586365
ho-ody <r4f4l135@gmail.com>
2023-05-10 20:21:45 +0200

    yoinked some code from github.com/isstaif, actually using only small part of it

---
+++ src/main/java/logic/Variable.java
@@ -0,0 +1,42 @@
+package logic;
+
+import java.util.Formatter;
+
+public class Variable {
+
+    private int stock;
+    private int required;
+    private double value;
+    public Variable(){
+        this.stock = 0;
+        this.required = 0;
+    }
+    public Variable(int stock, int required){
+        this.stock = stock;
+        this.required = required;
+    }
+    public int getStock() {
+        return stock;
+    }
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+    public int getRequired() {
+        return required;
+    }
+    public void setRequired(int required) {
+        this.required = required;
+    }
+    public double getValue() {
+        return value;
+    }
+    public void setValue(double value) {
+        this.value = value;
+    }
+    @Override
+    public String toString() {
+        Formatter f = new Formatter();
+        f.format("x[%d,%d]=%f", this.stock+1, this.required+1, this.value);
+        return f.toString();
+    }
+}
